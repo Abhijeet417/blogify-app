77552a49b358949128d8d1d445f50b273e08ba1f
Abhijeet SIngh <abspace417@gmail.com>
2024-04-24 11:51:13 +0530

    Some regular changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import transitions from "bootstrap";
 
 const Header = ({active,setActive,user,handleLogout}) => {
   const userId = user?.uid;
-  
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-light'>
         <div className="container-fluid bg-faded padding-media">
